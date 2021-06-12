5e3cd6ec2ff7dee27246327bb3033f8e5f830663
TheSpecialCoder <55451896+visheshdvivedi@users.noreply.github.com>
2021-06-12 11:26:42 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -10,12 +10,13 @@ def multiply(n1, n2):
 def divide(n1, n2):
     return n1 / n2
 
-print("This is calculator script\n")
+if __name__ == "__main__":
+    print("This is calculator script\n")
 
-print("Addition of 2 and 4:", add(2, 4))
-print("Substraction of 2 and 4:", subtract(2, 4))
-print("Multiplication of 2 and 4:", multiply(2, 4))
-print("Division of 2 and 4:", divide(2, 4))
+    print("Addition of 2 and 4:", add(2, 4))
+    print("Substraction of 2 and 4:", subtract(2, 4))
+    print("Multiplication of 2 and 4:", multiply(2, 4))
+    print("Division of 2 and 4:", divide(2, 4))
 
-print("\nValue of __name__:", __name__)
-print("\n")
+    print("\nValue of __name__:", __name__)
+    print("\n")
