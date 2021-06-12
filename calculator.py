607394c63bcfa82338ac320478e2de2bbe404315
TheSpecialCoder <55451896+visheshdvivedi@users.noreply.github.com>
2021-06-12 11:26:15 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,21 @@
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+print("This is calculator script\n")
+
+print("Addition of 2 and 4:", add(2, 4))
+print("Substraction of 2 and 4:", subtract(2, 4))
+print("Multiplication of 2 and 4:", multiply(2, 4))
+print("Division of 2 and 4:", divide(2, 4))
+
+print("\nValue of __name__:", __name__)
+print("\n")
---
+++ import_calculator.py
@@ -0,0 +1,8 @@
+import calculator
+
+print("This is import_calculator script")
+print("Addition of 2 and 4:", calculator.add(2, 4))
+print("Substraction of 2 and 4:", calculator.subtract(2, 4))
+print("Multiplication of 2 and 4:", calculator.multiply(2, 4))
+print("Division of 2 and 4:", calculator.divide(2, 4))
+print("\nValue of __name__:", calculator.__name__)
